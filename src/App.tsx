6bab3cd52a638939fd3a3ff1aded07c400ee8573
ALiuYiLin <scliuyilin@163.com>
2026-01-19 17:38:26 +0800

    feat: 回退app.tsx,作skill测试使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Button, Col, Flex, Modal, Row } from 'antd'
-import classNames from 'classnames'
 import './App.css'
 import { useBase } from './hooks/useBase'
 import { useDerived } from './hooks/useDerived'
@@ -15,38 +14,38 @@ function App() {
   return (
     <div className="">
       <Row gutter={20} >
-        {/* TO-CHECK 左队是否被选择 bg-yellow-400 */}
-        <Col span={12} className={classNames('bg-blue-400 h-50 relative', { 'bg-yellow-400': derived.isLeftSelected })}>
+        {/* TODO 左队是否被选择 bg-yellow-400 */}
+        <Col span={12} className='bg-blue-400 h-50 relative'>
           <Flex vertical>
-            {/* TO-CHECK 左队是否被选择 */}
-            <p>是否被选择：{derived.isLeftSelected ? '是' : '否'}</p>
-            {/* TO-CHECK 左队是否晋级 */}
-            <p>是否晋级：{derived.isLeftAdvanced ? '是' : '否'}</p>
-            {/* TO-CHECK 左队获取的投票数量*/}
-            <p>获得投票数量：{derived.leftVoteCount}</p>
-            {/* TO-CHECK 打开选择模态框 1 */}
-            {/* TO-CHECK 已投票不显示 */}
-            {!derived.isVoted && <Button onClick={() => options.openSelectModal(1)}>选择</Button>}
+            {/* TODO 左队是否被选择 */}
+            <p>是否被选择：{ }</p>
+            {/* TODO 左队是否晋级 */}
+            <p>是否晋级：{ }</p>
+            {/* TODO 左队获取的投票数量*/}
+            <p>获得投票数量：{}</p>
+            {/* TODO 打开选择模态框 1 */}
+            {/* TODO 已投票不显示 */}
+            <Button>选择</Button>
           </Flex>
         </Col>
-        {/* TO-CHECK 右队是否被选择 bg-yellow-400 */}
-        <Col span={12} className={classNames('bg-green-400 h-50 relative', { 'bg-yellow-400': derived.isRightSelected })}>
+        {/* TODO 右队是否被选择 bg-yellow-400 */}
+        <Col span={12} className='bg-green-400 h-50 relative'>
           <Flex vertical>
-            {/* TO-CHECK 右队是否被选择 */}
-            <p>是否被选择：{derived.isRightSelected ? '是' : '否'}</p>
-            {/* TO-CHECK 右队是否晋级 */}
-            <p>是否晋级：{derived.isRightAdvanced ? '是' : '否'}</p>
-             {/* TO-CHECK 右队获取的投票数量*/}
-            <p>获得投票数量：{derived.rightVoteCount}</p>
-            {/* TO-CHECK 打开选择模态框 2 */}
-            {/* TO-CHECK 已投票不显示 */}
-            {!derived.isVoted && <Button onClick={() => options.openSelectModal(2)}>选择</Button>}
+            {/* TODO 右队是否被选择 */}
+            <p>是否被选择：{ }</p>
+            {/* TODO 右队是否晋级 */}
+            <p>是否晋级：{ }</p>
+            {/* TODO 右队获取的投票数量*/}
+            <p>获得投票数量：{}</p>
+            {/* TODO 打开选择模态框 2 */}
+            {/* TODO 已投票不显示 */}
+            <Button>选择</Button>
           </Flex>
         </Col>
       </Row>
-      {/* TO-CHECK 选择模态框 */}
-      <Modal title="确认选择模态框" open={ui.showSelectModal} onCancel={options.closeSelectModal} onOk={options.comfirmSelect}>
-        <p>你选择了：{ui.tempSelectTeam === 1 ? '左队' : '右队'}</p>
+      {/* TODO 选择模态框 */}
+      <Modal title="确认选择模态框">
+        <p>内容</p>
       </Modal>
     </div>
   )
