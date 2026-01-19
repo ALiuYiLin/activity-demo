fbe3a4280cdeb38764a4e78578fa85b0cf9b393d
ALiuYiLin <scliuyilin@163.com>
2026-01-19 12:03:33 +0800

    feat: 添加skill使用案例

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from 'antd'
+import { Button, Col, Flex, Modal, Row } from 'antd'
 import './App.css'
 import { useBase } from './hooks/useBase'
 import { useDerived } from './hooks/useDerived'
 import { useOptions } from './hooks/useOptions'
 import { useUI } from './hooks/useUI'
-
-
 
 function App() {
   const base = useBase()
@@ -14,16 +12,35 @@ function App() {
   const options = useOptions({...base,...derived, ...ui})
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-8">
-      {/* TODO 打开选择队伍模态框 */}
-      <Button type="primary">open</Button>
-
-      {/* TODO 关闭选择队伍模态框 */}
-      <Button type="primary">close</Button>
-
-
-      {/* TODO 显示背景 左队是否晋级 show */}
-      <div className='bg'></div>
+    <div className="">
+      <Row gutter={20} >
+        {/* TODO 左队是否被选择 bg-yellow-400 */}
+        <Col span={12} className='bg-blue-400 h-50 relative'>
+          <Flex vertical>
+            {/* TODO 左队是否被选择 */}
+            <p>是否被选择：{}</p>
+            {/* TODO 左队是否晋级 */}
+            <p>是否晋级：{}</p>
+            {/* TODO 打开选择模态框 */}
+            <Button>选择</Button>
+          </Flex>
+        </Col>
+        {/* TODO 右队是否被选择 bg-yellow-400 */}
+        <Col span={12} className='bg-green-400 h-50 relative'>
+          <Flex vertical>
+            {/* TODO 右队是否被选择 */}
+            <p>是否被选择：{}</p>
+            {/* TODO 右队是否晋级 */}
+            <p>是否晋级：{}</p>
+            {/* TODO 打开选择模态框 */}
+            <Button>选择</Button>
+          </Flex>
+        </Col>
+      </Row>
+      {/* 选择模态框 */}
+      <Modal title="确认选择模态框">
+        <p>内容</p>
+      </Modal>
     </div>
   )
 }
